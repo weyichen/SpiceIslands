90321afa63df73972f64ee722252760b2de6fb99
Anand Sundaram <anchorflow@gmail.com>
2013-04-24 00:14:15 -0400

    Grooming
    Style cleanup and basic documentation

--- README.md
+++ README.md
@@ -3,10 +3,26 @@ Spice Islands
 
 Outline
 -------
-A turn-based, multiplayer strategy game in which multiple players compete to exploit the exotic resources of an archipelago within a fixed number of turns. All players start out in the same position on the map, which is a neutral zone. At first, the rest of the map is concealed by “fog of war.” Players must venture out into the high seas to explore the archipelago for resources that can be acquired and traded. Only spices can be sold for money, and the richest player wins at the end of the game, but other resources can help you collect spices more efficiently or thwart your rivals.
+Spice Islands is a turn-based, single player exploration game in which
+the player races to collect the exotic resources of an archipelago before time runs out.
+Each time the game begins, a random map of scattered islands is created. A ship sprite representing
+the player's vessel begins in a partly random location in the deep sea away from any specific island.
 
-Specific goals
--------
-- 2-5 players can play together sitting side-by-side and each taking turns, or have matches over the internet.
-- Randomly generate a new map with different geographical features and distribution of resources for every new game.
-- Character model and geographical feature graphics will be done using sprites.
+The player can navigate the ocean by clicking on the squares to which he or she desires to navigate.
+The ship can only move a few squares at a time. There is a limit on the number of turns available and the
+number of moves that the player can make per turn.
+
+When the player's mouse hovers over a square on the map, that square is evaluated as a potential travel destinnation.
+The square is framed in red if it is inaccessible because it is too far away from the ship or is landlocked 
+(the ship cannot enter the interior of islands). If the player clicks on a red square, nothing happens.
+If the destination is an accessible marine square, it is framed in yellow instead. If the player clicks on a yellow square,
+the ship will move to that square and the player will lose a number of remaining available moves corresponding to the distance
+traveled. Finally, if the destination is an accessible port square (on the border between an island and the ocean),
+the square is framed in green. If the player clicks on a green square, the ship will move to that location. Furthermore,
+if the player has visited this island for the first time, he or she will collect a randomly generated spice. If this spice
+causes the player's spice collection to contain the desired set of spices, victory is achieved. If the player plays through
+all available turns without collecting the necessary spices, the game is over and the player loses.
+
+A sidebar on the left of the map displays a list of the spices collected so far, the quantity of spices collected, and the
+names of the islands that the player has visited. The sidebar also reports the number of remaining moves available in this
+turn, as well as the number of turns remaining in the game.
--- new_gui.py
+++ new_gui.py
@@ -19,14 +19,14 @@ MOVESPTURN = 3
 
 # Graphical Parameters for Game Map
 FPS = 30 # frames per second, the general speed of the program
-WINDOWWIDTH = 640 # size of window's width in pixels
-WINDOWHEIGHT = 480 # size of windows' height in pixels
+MAP_WIDTH = 640 # size of window's width in pixels
+MAP_HEIGHT = 480 # size of windows' height in pixels
 MARGIN = 20
-BOXSIZE = 20 # size of box height & width in pixels
-BGCOLOR_LEVEL = 25
-ISLANDSIZE = 6 * BOXSIZE # size of island bounding box height & width in pixels
-BOARDWIDTH = (WINDOWWIDTH - 2 * MARGIN) / BOXSIZE # number of columns of icons
-BOARDHEIGHT = (WINDOWHEIGHT - 2 * MARGIN) / BOXSIZE # number of rows of icons
+BOX_SIZE = 20 # size of box height & width in pixels
+BG_COLOR_LEVEL = 25
+ISLAND_SIZE = 6 * BOX_SIZE # size of island bounding box height & width in pixels
+MAP_COLUMNS = (MAP_WIDTH - 2 * MARGIN) / BOX_SIZE # number of columns of icons
+MAP_ROWS = (MAP_HEIGHT - 2 * MARGIN) / BOX_SIZE # number of rows of icons
 # COLORS
 # NAME     (   R,   G,   B)
 OCEAN    = (  25, 135, 255)
@@ -37,13 +37,13 @@ YELLOW   = ( 255, 255,  0)
 # proper nouns
 SPICELIST = ["clove","cardomom","nutmeg","mace","anise",
         "cinnamon","pepper","cumin","camphor","fennel"]
-RESOURCELIST = ["sago","rice","tempeh"]
-ISLANDNAMES = ["Sumatra","Java","Sulawesi","Quezon","New Guinea",
-        "Bali","Singapore","Borneo", "Hawaii", "Madagascar", "Malabar", 
-        "Andaman", "Nicobar", "Trinidad", "Tobago", "Cuba", "Atlantis",
-        "Iceland", "Greenland", "Antarctica", "Australia", "Tasmania", 
-        "New Zealand", "Tahiti"]
         
+RESOURCELIST = ["sago","rice","tempeh"]
+
+ISLANDNAMES = ["Sumatra","Java","Sulawesi","Quezon","New Guinea",
+        "Bali", "Singapore", "Borneo", "Hawaii", "Madagascar", "Mauritius",
+        "Trinidad", "Tobago", "Atlantis", "Tasmania", "Tahiti"]
+
 # GUI Elements that need to be updated
 spice_table = gui.List(width=150, height=100)
 score_table = gui.List(width=150, height=100)
@@ -207,20 +207,20 @@ class MainGui(gui.Desktop):
         
         self.init(tbl, disp)
 
-        noise_w = ISLANDSIZE / 2
-        noise_h = ISLANDSIZE / 2
+        noise_w = ISLAND_SIZE / 2
+        noise_h = ISLAND_SIZE / 2
         noise_f = 3
         noise_o = 3
         
         p = PerlinNoiseGenerator()
         
         raw_ship_img = pygame.image.load(os.path.join('Images', 'sailboat.png'))
-        self.ship_img = pygame.transform.smoothscale(raw_ship_img, (BOXSIZE, BOXSIZE))
+        self.ship_img = pygame.transform.smoothscale(raw_ship_img, (BOX_SIZE, BOX_SIZE))
         
         ship_placed = False
         self.ship_pos = None
         
-        noise = np.array(p.generate_noise(ISLANDSIZE, ISLANDSIZE, 1.5, 3))
+        noise = np.array(p.generate_noise(ISLAND_SIZE, ISLAND_SIZE, 1.5, 3))
         rg = np.zeros(noise.shape, dtype=np.int8)
         rg.fill(25)
         bg = np.transpose(np.array([rg, rg, noise]))
@@ -229,19 +229,19 @@ class MainGui(gui.Desktop):
         self.islands = {}
         num_islands = 0
         
-        for x in range(WINDOWWIDTH / ISLANDSIZE):
-            for y in range(WINDOWHEIGHT / ISLANDSIZE):
-                (left, top) = (x * ISLANDSIZE, y * ISLANDSIZE)
+        for x in range(MAP_WIDTH / ISLAND_SIZE):
+            for y in range(MAP_HEIGHT / ISLAND_SIZE):
+                (left, top) = (x * ISLAND_SIZE, y * ISLAND_SIZE)
                 if random.randint(1,3) > 1 and (x + y) % 2 == 0 and num_islands < len(ISLANDNAMES):
                     world = map.Map('Island', 
                         IslandGenerator().generate_island(noise_w, noise_h, noise_f, noise_o))
                     world.draw_minimap()
-                    self.drape(world.minimap, (left, top))
-                    island_rect = pygame.Rect(left, top, ISLANDSIZE, ISLANDSIZE)
+                    self.draw_surface(world.minimap, (left, top))
+                    island_rect = pygame.Rect(left, top, ISLAND_SIZE, ISLAND_SIZE)
                     self.islands[ISLANDNAMES[num_islands]] = (False, island_rect)
                     num_islands += 1
                 else:
-                    self.drape(water, (left, top))
+                    self.draw_surface(water, (left, top))
                     if not ship_placed:
                         self.ship_pos = (left + MARGIN, top + MARGIN)
                         ship_placed = True
@@ -250,7 +250,7 @@ class MainGui(gui.Desktop):
 
     def run(self):
         global NUMTURNS, moves_left
-        screen_rect = self.get_render_area()
+        screen_rect = self.get_map_area()
         
         self.init()
         FPSCLOCK = pygame.time.Clock()
@@ -260,9 +260,9 @@ class MainGui(gui.Desktop):
         # only run main game loop when there is no dialog
         while True and not dialog_on:
             mouse_clicked = False
-            box_x, box_y = None, None
-            self.plaster(self.canvas)
-            self.drape(self.ship_img, self.ship_pos)
+            map_tile_x, map_tile_y = None, None
+            self.draw_pixel_array(self.canvas)
+            self.draw_surface(self.ship_img, self.ship_pos)
             
             for event in pygame.event.get(): # event handling loop
                 self.event(event)
@@ -273,16 +273,16 @@ class MainGui(gui.Desktop):
                     mouse_x, mouse_y = event.pos
                 elif event.type == MOUSEBUTTONUP:
                     # only get map position if we clicked inside map
-                    screen_rect = self.get_render_area()
+                    screen_rect = self.get_map_area()
                     mouse_x, mouse_y = event.pos
                     if mouse_x > screen_rect.x:
                         mouse_clicked = True
             
-            box_x, box_y = self.get_box_at_pixel(mouse_x, mouse_y)
-            if box_x != None and box_y != None and NUMTURNS > 0:
-                self.highlight_border(box_x, box_y, self.ship_pos, self.canvas)
+            map_tile_x, map_tile_y = self.get_map_tile_at_pixel(mouse_x, mouse_y)
+            if map_tile_x != None and map_tile_y != None and NUMTURNS > 0:
+                self.highlight_border(map_tile_x, map_tile_y, self.ship_pos, self.canvas)
                 # The mouse is currently over a box.
-                left, top = self.box_corner(box_x, box_y)
+                left, top = self.map_tile_corner(map_tile_x, map_tile_y)
                 if self.is_reachable(left, top, self.ship_pos, self.canvas) and mouse_clicked:
                         # decrement number of moves by the distance traveled
                         moves_left-= self.block_distance(left, top, self.ship_pos)
@@ -297,7 +297,7 @@ class MainGui(gui.Desktop):
                         self.game_area.repaint()
                         
                         # Report spices collected and update visited islands list
-                        nearby = self.nearby_islands(box_x, box_y, self.canvas)
+                        nearby = self.nearby_islands(map_tile_x, map_tile_y, self.canvas)
                         if nearby:
                             for island in nearby:
                                 info = self.islands[island]
@@ -330,7 +330,7 @@ class MainGui(gui.Desktop):
     def block_distance(self, left, top, ship_pos):
         destination = np.array([left, top])
         current_location = np.array(ship_pos)
-        return int(np.linalg.norm(destination - current_location) / BOXSIZE)
+        return int(np.linalg.norm(destination - current_location) / BOX_SIZE)
     
     def is_reachable(self, left, top, ship_pos, canvas):
         slice = canvas[left:left+39,top:top+39, :-1]
@@ -338,27 +338,27 @@ class MainGui(gui.Desktop):
         return dist <= moves_left and not self.is_island(slice)
     
     def is_island(self, slice):
-        return np.sum(slice != BGCOLOR_LEVEL) > 3 * np.size(slice) / 8
+        return np.sum(slice != BG_COLOR_LEVEL) > 3 * np.size(slice) / 8
     
     def nearby_islands(self, x, y, canvas):
         islands_here = []
-        for box_x in range(max(0, x-1), min(BOARDWIDTH, x + 2)):
-            for box_y in range(max(0, y-1), min(BOARDHEIGHT, y + 2)):
-                if (box_x == x or box_y == y) :
-                    (left, top) = self.box_corner(box_x, box_y)
+        for map_tile_x in range(max(0, x-1), min(MAP_COLUMNS, x + 2)):
+            for map_tile_y in range(max(0, y-1), min(MAP_ROWS, y + 2)):
+                if (map_tile_x == x or map_tile_y == y) :
+                    (left, top) = self.map_tile_corner(map_tile_x, map_tile_y)
                     slice = canvas[left:left+39,top:top+39, :-1]
                     if self.is_island(slice):
-                        box_rect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+                        box_rect = pygame.Rect(left, top, BOX_SIZE, BOX_SIZE)
                         for island in self.islands:
                             island_rect = self.islands[island][1]
                             if island_rect.colliderect(box_rect):
                                 islands_here.append(island)
         return islands_here
     
-    def highlight_border(self, box_x, box_y, ship_pos, canvas):
-        left, top = self.box_corner(box_x, box_y)
+    def highlight_border(self, map_tile_x, map_tile_y, ship_pos, canvas):
+        left, top = self.map_tile_corner(map_tile_x, map_tile_y)
         if self.is_reachable(left, top, ship_pos, canvas):
-            if self.nearby_islands(box_x, box_y, canvas):
+            if self.nearby_islands(map_tile_x, map_tile_y, canvas):
                 color = GREEN
             else:
                 color = YELLOW
@@ -366,35 +366,32 @@ class MainGui(gui.Desktop):
             color = RED
         
         pygame.draw.rect(self.game_area.image_buffer, color, 
-                (left - 5, top - 5, BOXSIZE + 10, BOXSIZE + 10), 4)
+                (left - 5, top - 5, BOX_SIZE + 10, BOX_SIZE + 10), 4)
         return
 
-    def box_corner(self, box_x, box_y):
+    def map_tile_corner(self, map_tile_x, map_tile_y):
         # Convert board coordinates to pixel coordinates
-        left = box_x * BOXSIZE + MARGIN
-        top = box_y * BOXSIZE + MARGIN
+        left = map_tile_x * BOX_SIZE + MARGIN
+        top = map_tile_y * BOX_SIZE + MARGIN
         return (left, top) 
 
-    def get_box_at_pixel(self, x, y):
-        screen_rect = self.get_render_area()
-        for box_x in range(BOARDWIDTH):
-            for box_y in range(BOARDHEIGHT):
-                left, top = self.box_corner(box_x, box_y)
-                box_rect = pygame.Rect(left, top, BOXSIZE, BOXSIZE)
+    def get_map_tile_at_pixel(self, x, y):
+        screen_rect = self.get_map_area()
+        for map_tile_x in range(MAP_COLUMNS):
+            for map_tile_y in range(MAP_ROWS):
+                left, top = self.map_tile_corner(map_tile_x, map_tile_y)
+                box_rect = pygame.Rect(left, top, BOX_SIZE, BOX_SIZE)
                 if box_rect.collidepoint(x - screen_rect.x, y - screen_rect.y):
-                    return (box_x, box_y)
+                    return (map_tile_x, map_tile_y)
         return (None, None)
     
-    def get_render_area(self):
+    def get_map_area(self):
         return self.game_area.get_abs_rect()
         
-    def get_canvas(self):
-        return pygame.surfarray.array3d(self.game_area.image_buffer)
-        
-    def drape(self, surf, pos):
+    def draw_surface(self, surf, pos):
         self.game_area.image_buffer.blit(surf, pos)
         
-    def plaster(self, canvas):
+    def draw_pixel_array(self, canvas):
         pygame.surfarray.blit_array(self.game_area.image_buffer, canvas)
 
 def main():
